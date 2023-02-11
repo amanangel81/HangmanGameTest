4e2f74bed7af29eefc8d2b6dd57eddfd9412b309
amanarvino <amanarvino@gmail.com>
2023-02-10 18:22:37 -0700

    changed print statements

--- Hangman.py
+++ Hangman.py
@@ -19,7 +19,7 @@ def hangman(word):
     print("Welcome to Hangman")
     while wrong < len(stages) - 1:
         print("\n")
-        msg = "Guess a letter: "
+        msg = "Good luck guessing: "
         char = input(msg)
         if char in rletters:
             cind = rletters.index(char)
@@ -31,13 +31,13 @@ def hangman(word):
         e = wrong + 1
         print("\n".join(stages[0: e]))
         if "_" not in board:
-            print("You win!")
+            print("Good job, that was easy though!")
             print(" ".join(board))
             win = True
             break
     if not win:
         print("\n".join(stages[0: wrong]))
-        print("You lose! It was {}.".format(word))
+        print("Try again fam! It was {}.".format(word))
 
 words = []
 with open("word_library.txt", "r") as file:
